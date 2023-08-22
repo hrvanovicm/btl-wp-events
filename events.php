11431a71858f763eae77d46ed0227eccfcb9571c
Mirza Hrvanovic <mirza.hrvanovic@edu.fit.ba>
2023-08-22 22:02:52 +0200

    initial

---
+++ events.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Events
+Plugin URI: https://www.github.com/hrvanovic/btl-wp-events
+Description: Simple plugin for creating events.
+Version: 1.0
+Author: Mirza Hrvanovic
+Author URI:  https://www.github.com/hrvanovic
+License: MIT License
+License URI: https://opensource.org/licenses/MIT
+*/
